c130aa105c6911143669fbfd385c3769e2445dce
malteshkammar <malteshkammar4@gmail.com>
2022-09-17 12:52:12 +0530

    college management project

---
+++ Common/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.E Civil Engineering</a></li>
+              <li><a href="">B.E Electrical Engineering</a></li>
+              <li><a href="">B.E Mechanical Engineering</a></li>
+              <li><a href="">B.E Computer Science</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p><Address:Hubli></Address:Hubli></p>
+              <p>Telephones: </p>
+              <p>E-mail: kleit.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © Alright Reserve 2022</p>
+</footer>
